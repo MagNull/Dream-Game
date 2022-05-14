c3929f288d8a12e67aa7d89c6a6b30032b856a49
SlivnyiArtem <slivnay1965@gmail.com>
2022-05-14 23:27:47 +0500

    fix bugs

--- Assets/TimeMagicFormBehaviour.cs
+++ Assets/TimeMagicFormBehaviour.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class TimeMagicFormBehaviour : MonoBehaviour
 {
     private static bool _isTimeShiftActivatePrevious;
+    private static bool _isInForm;
+    //private static bool 
     private static float _formTimer;
     private static float _coolDawnTimer;
     private static bool _isCoolDawn;
@@ -15,18 +17,29 @@ public class TimeMagicFormBehaviour : MonoBehaviour
         _formTimer = 0f;
         _coolDawnTimer = 0f;
         _isCoolDawn = false;
+        _isInForm = false;
     }
 
     void Update()
     {
         //print(_formTimer + "f");
         //print(_coolDawnTimer + "c");
-        _formTimer -= Time.deltaTime;
-        _coolDawnTimer -= Time.deltaTime;
-        if (_formTimer <= 0)
+        if (_formTimer >0)
+            _formTimer -= Time.deltaTime;
+        if (_coolDawnTimer > 0)
+            _coolDawnTimer -= Time.deltaTime;
+        if (_formTimer <= 0 && _isInForm)
+        {
+            _formTimer = 0f;
+            _isInForm = false;
             OnChange();
-        if (_coolDawnTimer <= 0)
+        }
+
+        if (_coolDawnTimer <= 0 && _isCoolDawn)
+        {
+            _coolDawnTimer = 0f;
             _isCoolDawn = false;
+        }
     }
 
     public static void OnChange()
@@ -37,14 +50,16 @@ public class TimeMagicFormBehaviour : MonoBehaviour
             TimeShiftConstants.UpdateAllExceptPlayer(1);
             _coolDawnTimer = 1f;
             //_isTimeShiftActivatePrevious = !_isTimeShiftActivatePrevious;
+            _isTimeShiftActivatePrevious = false;
             _isCoolDawn = true;
         }
-        else if (!_isCoolDawn)
+        else if (!_isCoolDawn && !_isTimeShiftActivatePrevious)
         {
             print("activate");
             TimeShiftConstants.UpdateAllExceptPlayer(0);
             _formTimer = 1f;
-            _isTimeShiftActivatePrevious = !_isTimeShiftActivatePrevious;
+            _isInForm = true;
+            _isTimeShiftActivatePrevious = true;
         }
     }
 }
