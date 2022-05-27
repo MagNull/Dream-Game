af2391074ab4471edcc5321d101c5ae132f1599c
MagNull <pathfo@yandex.ru>
2022-05-27 14:22:20 +0500

    Clean magic state

--- Assets/Source/Runtime/Slime Components/Slime State/MagicState.cs
+++ Assets/Source/Runtime/Slime Components/Slime State/MagicState.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class MagicState : SlimeState
 {
-    [SerializeField]
-    private float _gravitationReduceCoefficient;
-    private Rigidbody2D _rigidbody2D;
     private bool _isAbilityActive;
     public override void ActivateAbility()
     {
-        if (_isAbilityActive)
-            TimeShiftConstants.SetOtherConstant(1);
-        else
-            TimeShiftConstants.SetOtherConstant(0);
+        TimeShiftConstants.SetOtherConstant(_isAbilityActive ? 1 : 0);
         _isAbilityActive = !_isAbilityActive;
     }
     public override int GetDamageModificator(object source) => 1;
 
-    public override void Init(GameObject slimeGameObject)
-    {
-        if (slimeGameObject.TryGetComponent(out Rigidbody2D rigidbody2D))
-            _rigidbody2D = rigidbody2D;
-        else
-            throw new Exception("Incorrect Air state initialization");
-    }
+    public override void Init(GameObject slimeGameObject) { }
 
     public override void Exit()
     {
