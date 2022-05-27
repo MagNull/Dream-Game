118df0cfac6e9525b249cfa8d2975f91a0f1145a
MagNull <pathfo@yandex.ru>
2022-05-27 14:18:07 +0500

    Add menu + pause code

---
+++ Assets/Source/Runtime/UI & HUD/Pause.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Source.Runtime.UI___HUD
+{
+    public class Pause : MonoBehaviour
+    {
+        public void ChangePauseState()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+            Time.timeScale = gameObject.activeSelf ? 1 : 0;
+        }
+    }
+}
