80b9ee2813852c1d61e3a5739955046ad5909a62
MagNull <pathfo@yandex.ru>
2022-05-28 13:38:12 +0500

    Implement pause

--- Assets/Source/Runtime/Input/PlayerInput.cs
+++ Assets/Source/Runtime/Input/PlayerInput.cs
@@ -132,6 +132,34 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""UI"",
+            ""id"": ""4dc860f0-68bd-49f0-b674-5f7bcfce49a7"",
+            ""actions"": [
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""f4e1c438-70e0-47e6-8cb4-aabd907b7013"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d950c71b-b2fe-4695-ac81-1caad601620f"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -142,6 +170,9 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         m_Slime_ActivateAbility = m_Slime.FindAction("Activate Ability", throwIfNotFound: true);
         m_Slime_ChangeState = m_Slime.FindAction("Change State", throwIfNotFound: true);
         m_Slime_Jump = m_Slime.FindAction("Jump", throwIfNotFound: true);
+        // UI
+        m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
+        m_UI_Pause = m_UI.FindAction("Pause", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -254,11 +285,48 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         }
     }
     public SlimeActions @Slime => new SlimeActions(this);
+
+    // UI
+    private readonly InputActionMap m_UI;
+    private IUIActions m_UIActionsCallbackInterface;
+    private readonly InputAction m_UI_Pause;
+    public struct UIActions
+    {
+        private @PlayerInput m_Wrapper;
+        public UIActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Pause => m_Wrapper.m_UI_Pause;
+        public InputActionMap Get() { return m_Wrapper.m_UI; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(UIActions set) { return set.Get(); }
+        public void SetCallbacks(IUIActions instance)
+        {
+            if (m_Wrapper.m_UIActionsCallbackInterface != null)
+            {
+                @Pause.started -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+            }
+            m_Wrapper.m_UIActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
+            }
+        }
+    }
+    public UIActions @UI => new UIActions(this);
     public interface ISlimeActions
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnActivateAbility(InputAction.CallbackContext context);
         void OnChangeState(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+    }
+    public interface IUIActions
+    {
+        void OnPause(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/Source/Runtime/Pause.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Source.Runtime
+{
+    public class Pause : MonoBehaviour
+    {
+        public void ChangePauseState()
+        {
+            gameObject.SetActive(!gameObject.activeSelf); //Пока так, потом можно сделать плавно
+            Time.timeScale = gameObject.activeSelf ? 0 : 1;
+        }
+    }
+}
