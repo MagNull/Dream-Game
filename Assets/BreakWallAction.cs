8ee4e60c9e3c572ea9d8f01cec10204dc1571e5f
kretatusha <92570098+kretatusha@users.noreply.github.com>
2022-06-04 15:26:38 +0500

    ADD хрупкий блок

--- Assets/BreakWallAction.cs
+++ Assets/BreakWallAction.cs
@@ -4,21 +4,54 @@ using Source.Slime_Components;
 using UnityEngine;
 
 public class BreakWallAction : MonoBehaviour
-{
-    public void Init()
-    {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent(out Slime slime))
+{    
+    Rigidbody2D rb;
+    public Slime slime_object;
+    private bool _isTurnOn;
+    private bool _isNear;
+    
+    void Awake()
         {
-            foreach (Transform child in transform)
-            {
-                var childRigidbody2D = child.gameObject.GetComponent<Rigidbody2D>();
-                childRigidbody2D.gravityScale = 10;
-            }
+            _isTurnOn = false;
+            _isNear = false;
+            rb = GetComponent<Rigidbody2D>();
         }
-    }
+        
+      private void OnTriggerStay2D(Collider2D collision)
+           {
+                if(collision.TryGetComponent(out Slime slime))
+                    _isNear = true;
+           }
+        
+      private void OnTriggerExit2D(Collider2D collision)
+           {
+                if (collision.TryGetComponent(out Slime slime))
+                    _isNear = false;
+           }
+           
+      private void Update()
+            {
+            print(_isNear);
+                 if(_isNear && !_isTurnOn)
+                 {
+                float weight = slime_object.GetWeight();
+                int i = 1;
+                print("weight is"+weight);
+                print(weight > 1);
+                if (weight > 1)
+                {
+                _isTurnOn = true;
+                rb.simulated = false;
+                    foreach (Transform child in transform)
+                    {
+                        child.position += Vector3.left * 0.3f * i;
+                        i *= -1;
+                        var childRigidbody2D = child.gameObject.GetComponent<Rigidbody2D>();
+                        childRigidbody2D.gravityScale = 10;
+                     }
+                 }
+              }
 }
+}
+                 
+
---
+++ Assets/Scripts/Buttin.cs
@@ -0,0 +1,42 @@
+﻿using Source.Slime_Components;
+using UnityEngine;
+
+public class Buttin : MonoBehaviour
+{
+    public GameObject _gameObject;
+    public Slime slime_object;
+    private bool _isTurnOn;
+    private bool _isNear;
+
+    void Awake()
+    {
+        _isTurnOn = false;
+        _isNear = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Slime slime))
+        {
+            _isNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Slime slime))
+        {
+            _isNear = false;
+        }
+    }
+
+    private void Update()
+    {   
+        float weight = slime_object.GetWeight();
+        if (_isNear && !_isTurnOn && weight > 1)
+        {
+            _isTurnOn = !_isTurnOn;
+            _gameObject.gameObject.SetActive(!_gameObject.activeSelf);
+        }
+    }
+}
